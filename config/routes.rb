648304ba2424e5ba9e6f3125a94ec7c3385aabc3
Muresan Ionut <muresan.ionut95@gmail.com>
2015-06-16 12:03:08 +0300

    active_admin wq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'web/index'
-	root 'web#index'
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
