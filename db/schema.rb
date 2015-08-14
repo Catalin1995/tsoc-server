4d95e709528366de5463c07b0be5702dac52a945
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-14 17:22:55 +0300

    create model for link and skill

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707110011) do
+ActiveRecord::Schema.define(version: 20150814141356) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -43,6 +43,30 @@ ActiveRecord::Schema.define(version: 20150707110011) do
     t.datetime "updated_at",  null: false
     t.string   "image"
     t.string   "link"
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.integer  "skill_id"
+    t.string   "label"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "maxPoints"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "stats", force: :cascade do |t|
+    t.integer  "skill_id"
+    t.integer  "value"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
