1f3812be7227248182b83df7f0345751eb94f939
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-14 15:18:06 +0300

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,6 @@ set :repo_url, 'https://github.com/Catalin1995/tsoc-server.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/internship/tsoc-server'
 
 # Default value for :scm is :git
 # set :scm, :git
