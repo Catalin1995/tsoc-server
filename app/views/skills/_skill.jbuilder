5bb3e7e7d36770530410d3527571acd3c4a2887f
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-17 12:45:34 +0300

    return statistics, rankDescriptions and links only if exist

--- app/views/skills/_skill.jbuilder
+++ app/views/skills/_skill.jbuilder
@@ -1,3 +1,16 @@
 json.(skill, :id, :title, :description, :maxPoints, :created_at, :updated_at)
-json.links Link.where(:skill_id => skill.id)
-json.stats Statistic.where(:skill_id => skill.id)
+
+links = Link.where(:skill_id => skill.id)
+if links != nil
+  json.links links
+end
+
+stats = Statistic.where(:skill_id => skill.id)
+if stats != nil
+  json.stats stats
+end
+
+rankDescriptions = Rank.where(:skill_id => skill.id)
+if rankDescriptions != nil
+  json.rankDescriptions rankDescriptions
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,16 @@ Link.create!(skill_id: skill.id, label: 'Sublime Text 3, a great code editor', u
 Statistic.create!(skill_id: skill.id, title: 'Intellect', value: 1)
 Statistic.create!(skill_id: skill.id, title: 'Charisma', value: 1)
 Statistic.create!(skill_id: skill.id, title: 'Strength', value: 1)
+Rank.create!(skill_id: skill.id, description: 'You understand how to create and view a basic web page.')
+Rank.create!(skill_id: skill.id, description: 'You understand how to link pages together, design multi-column layouts, and work with form fields and media elements.')
 
 #Skill 2
 skill = Skill.create!(title: 'CSS', description: 'Cascading Style Sheets (CSS) is a language for styling web pages.  CSS rules target elements in the HTML document to specify the presentation, such as font, color, spacing, and size.', maxPoints: 2)
 Link.create!(skill_id: skill.id, label: 'CSS Tutorials', url: 'http://www.htmldog.com/guides/css/')
 Link.create!(skill_id: skill.id, label: 'Can I use... (browser support)', url: 'http://caniuse.com/#cats=CSS')
 Statistic.create!(skill_id: skill.id, title: 'Charisma', value: 3)
+Rank.create!(skill_id: skill.id, description: 'You are familiar with the basics of CSS formatting and the box model.')
+Rank.create!(skill_id: skill.id, description: 'You are familiar with media queries and responsive design for adapting your design to fit different devices.')
 
 #Skill 3
 skill = Skill.create!(title: 'CSS Tools', maxPoints: 2, description: 'Preprocessors like LESS and SASS help you write more structured and efficient CSS by adding features like variables, functions, and nesting.')
@@ -28,6 +32,24 @@ Link.create!(skill_id: skill.id, label: 'Sass', url: 'http://sass-lang.com/')
 Link.create!(skill_id: skill.id, label: 'Stylus', url: 'http://learnboost.github.io/stylus/')
 Statistic.create!(skill_id: skill.id, title: 'Charisma', value: 2)
 Statistic.create!(skill_id: skill.id, title: 'Dexterity', value: 1)
+Rank.create!(skill_id: skill.id, description: 'You are familiar with variables and nesting.')
+Rank.create!(skill_id: skill.id, description: 'You can use advanced tools like mix-ins and sometimes write your own.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 User.create!(email: 'admin@example.com', password: 'password', name: 'admin', password_confirmation: 'password', mentor: true)
 User.create!(email: 'mentor1@example.com', password: 'password', name: 'mentor1', password_confirmation: 'password', mentor: true)
