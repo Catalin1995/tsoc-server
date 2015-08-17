6c7b034499b9a56619c2ee124c26ac5fd6577652
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-17 11:29:17 +0300

    create skills in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#Skill 1
+skill = Skill.create!(title: 'HTML', description: 'The main language for creating web pages, HTML is written in the form of tags enclosed in angle brackets (like &lt;html&gt;).', maxPoints: 2)
+Link.create!(skill_id: skill.id, label: 'HTML.net Tutorials', url: 'http://www.html.net/tutorials/html/')
+Link.create!(skill_id: skill.id, label: 'Sublime Text 3, a great code editor', url: 'http://www.sublimetext.com/')
+Statistic.create!(skill_id: skill.id, title: 'Intellect', value: 1)
+Statistic.create!(skill_id: skill.id, title: 'Charisma', value: 1)
+Statistic.create!(skill_id: skill.id, title: 'Strength', value: 1)
+
+#Skill 2
+skill = Skill.create!(title: 'CSS', description: 'Cascading Style Sheets (CSS) is a language for styling web pages.  CSS rules target elements in the HTML document to specify the presentation, such as font, color, spacing, and size.', maxPoints: 2)
+Link.create!(skill_id: skill.id, label: 'CSS Tutorials', url: 'http://www.htmldog.com/guides/css/')
+Link.create!(skill_id: skill.id, label: 'Can I use... (browser support)', url: 'http://caniuse.com/#cats=CSS')
+Statistic.create!(skill_id: skill.id, title: 'Charisma', value: 3)
+
+#Skill 3
+skill = Skill.create!(title: 'CSS Tools', maxPoints: 2, description: 'Preprocessors like LESS and SASS help you write more structured and efficient CSS by adding features like variables, functions, and nesting.')
+Link.create!(skill_id: skill.id, label: 'Sass vs. LESS', url: 'http://css-tricks.com/sass-vs-less/')
+Link.create!(skill_id: skill.id, label: 'LESS', url: 'http://lesscss.org/')
+Link.create!(skill_id: skill.id, label: 'Sass', url: 'http://sass-lang.com/')
+Link.create!(skill_id: skill.id, label: 'Stylus', url: 'http://learnboost.github.io/stylus/')
+Statistic.create!(skill_id: skill.id, title: 'Charisma', value: 2)
+Statistic.create!(skill_id: skill.id, title: 'Dexterity', value: 1)
+
 User.create!(email: 'admin@example.com', password: 'password', name: 'admin', password_confirmation: 'password', mentor: true)
 User.create!(email: 'mentor1@example.com', password: 'password', name: 'mentor1', password_confirmation: 'password', mentor: true)
 User.create!(email: 'mentor2@example.com', password: 'password', name: 'mentor2', password_confirmation: 'password', mentor: true)
