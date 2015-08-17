690966f8509733b26114ab6031ad211ce95f2a79
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-17 11:23:52 +0300

    skill has_many links skill has_many statistics

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,7 +1,8 @@
 class Skill < ActiveRecord::Base
+  has_many :statistics
+  has_many :links
 
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :maxPoints, presence: true
-
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :maxPoints, presence: true
 end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,7 +1,7 @@
 class Statistic < ActiveRecord::Base
-
+  belongs_to :skill
+  
   validates :title, presence: true
   validates :value, presence: true
   validates :skill, presence: true
-
 end
