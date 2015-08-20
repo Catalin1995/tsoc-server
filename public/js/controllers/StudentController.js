562c4e9610b0954540f2e26e03f1497cec0022c3
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-20 15:33:45 +0300

    SET NAME   delete css code for name   change method to list name   delete condition for set_avatarName from useHash
    ADD EMAIl
    
    PORTRAIT
      remove method for generate random name
      detele methods for change portrait from js
      portrat_url = user_image_url
      delete condition for set portrait from useHash method
    
    HASH
      remove portrait from hash
      remove updateHash function
      remove window.onhashchange function
      remove hash.subscribe function
      remove self.useHash

--- public/js/controllers/StudentController.js
+++ public/js/controllers/StudentController.js
@@ -4,62 +4,66 @@ app.controller('StudentController', function ($scope, $http, $routeParams) {
 
   $http.get('/api/student/'+$routeParams.id).success(function(data){
     $scope.student = data['body'];
-  });
 
-  $http.get('api/skills').success(function(data){
-    $scope.skils = data['body'];
+    $http.get('api/skills').success(function(data){
+      $scope.all_skills = data['body'];
 
-    $scope.skills = {
-      learnTemplate: 'Learn {n} to unlock.',
-      portraitPathTemplate: 'img/portraits/portrait-{n}.jpg',
-      numPortraits: 22,
-      defaultStats: {
-        'Charisma': 9
-        , 'Dexterity': 9
-        , 'Fortitude': 9
-        , 'Intellect': 9
-        , 'Strength': 9
-        , 'Wisdom': 9
-      },
-      skills: $scope.skils
-    };
+      $scope.skills = {
+        learnTemplate: 'Learn {n} to unlock.',
+        portraitUrl: 'img/portraits/portrait-1.jpg',
+        numPortraits: 22,
+        myName: $scope.student.name,
+        emailAddress: $scope.student.email,
+        hash: '_a2b2cdef_2_iasd',
+        defaultStats: {
+          'Charisma': 9
+          , 'Dexterity': 9
+          , 'Fortitude': 9
+          , 'Intellect': 9
+          , 'Strength': 9
+          , 'Wisdom': 9
+        },
+        skills: $scope.all_skills
+      };
 
-    //konami code plugin
-    (function ($) {
+      //konami code plugin
+      (function ($) {
 
-      $.fn.konami = function (callback, code) {
-        if (code == undefined) code = "38,38,40,40,37,39,37,39,66,65"; //Super secret!
+        $.fn.konami = function (callback, code) {
+          if (code == undefined) code = "38,38,40,40,37,39,37,39,66,65"; //Super secret!
 
-        return this.each(function () {
-          var kkeys = [];
-          $(this).keydown(function (e) {
-            kkeys.push(e.keyCode);
-            if (kkeys.toString().indexOf(code) >= 0) {
-              $(this).unbind('keydown', arguments.callee);
-              callback(e);
-            }
+          return this.each(function () {
+            var kkeys = [];
+            $(this).keydown(function (e) {
+              kkeys.push(e.keyCode);
+              if (kkeys.toString().indexOf(code) >= 0) {
+                $(this).unbind('keydown', arguments.callee);
+                callback(e);
+              }
+            });
           });
+        }
+
+      })(jQuery);
+
+      //Custom closure
+      (function($, ko, data){
+
+        //On page load
+        $(function(){
+
+          //Create and bind the viewmodel
+          var vm = new tft.skilltree.Calculator(data);
+          ko.applyBindings(vm);
+
+          //apply konami code plugin
+          // $(window).konami(function () { vm.open(); vm.godMode(); });
+
         });
-      }
+        console.log($scope.skills)
+      })(window.jQuery, window.ko, $scope.skills);
+    });
 
-    })(jQuery);
-
-    //Custom closure
-    (function($, ko, data){
-
-      //On page load
-      $(function(){
-
-        //Create and bind the viewmodel
-        var vm = new tft.skilltree.Calculator(data);
-        ko.applyBindings(vm);
-
-        //apply konami code plugin
-        $(window).konami(function () { vm.open(); vm.godMode(); });
-
-      });
-
-    })(window.jQuery, window.ko, $scope.skills);
   });
 
 });
