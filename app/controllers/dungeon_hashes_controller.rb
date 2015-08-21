2b9553d36029cd222662fdc2d214369f563018e2
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-21 15:11:45 +0300

    automaticaly created if dungeon_hash does not exist

--- app/controllers/dungeon_hashes_controller.rb
+++ app/controllers/dungeon_hashes_controller.rb
@@ -1,5 +1,12 @@
 class DungeonHashesController < ApplicationController
   def index
     @dungeon_hash = DungeonHash.where(:dungeon_id => params[:dungeon_id], :user_id => params[:student_id]).first
+    if @dungeon_hash == nil
+      if Dungeon.exists?(params[:dungeon_id])
+        if User.exists?(params[:student_id])
+          @dungeon_hash = DungeonHash.create!(:dungeon_id => params[:dungeon_id], :user_id => params[:student_id], :dungeon_hash => '_')
+        end
+      end
+    end
   end
 end
