ddc92cbb20179c900a2aa66b1c35eaa468a68753
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-25 11:20:05 +0300

    create tests for StudentController

---
+++ spec/controllers/student_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe StudentController, type: :controller do
+  render_views
+  describe 'GET index' do
+    it 'when does not exist items in db' do
+      get :index, format: :json
+      expect(json[:code]).to eq(200)
+      expect(json[:body]).to eq([])
+    end
+
+    it 'when exist items in db' do
+      create :user, mentor: false, email: 'admin@example.com'
+      create :user, mentor: false, email: 'test@test.com'
+      create :user, mentor: true, email: 'test@example.com'
+
+      get :index, format: :json
+      expect(json[:code]).to eq(200)
+      expect(json[:body].count).to eq(2)
+    end
+  end
+
+  describe 'GET show' do
+    it 'when id does not exist' do
+      expect do
+        get :show, id: -1
+      end.to raise_error ActiveRecord::RecordNotFound
+    end
+
+    it 'when id exist' do
+      mentor = create :user, mentor: true, email: 'admin@example.com'
+      get :show, id: mentor.id, format: :json
+      expect(json['code']).to eq(200)
+      expect(json['body']['id']).to eq(mentor.id)
+      expect(json['body']['email']).to eq(mentor.email)
+      expect(json['body']['name']).to eq(mentor.name)
+    end
+
+    it 'when id exist but is mentor' do
+      mentor = create :user, mentor: false, email: 'admin@example.com'
+      get :show, id: mentor.id, format: :json
+      expect(json['code']).to eq(200)
+      expect(json['body']['id']).to eq(mentor.id)
+      expect(json['body']['email']).to eq(mentor.email)
+      expect(json['body']['name']).to eq(mentor.name)
+    end
+  end
+end
