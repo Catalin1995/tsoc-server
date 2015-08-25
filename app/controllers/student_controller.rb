f0bed1e7cb7f9bbe0751e1bbcf522c9fb29ceed0
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-25 11:11:33 +0300

    remove course from StudentController

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -5,16 +5,5 @@ class StudentController < ApplicationController
 
   def show
     @student = User.find(params['id'])
-    @badges = Badge.where(student_id: params['id'])
-    @status = @badges.count
-    @number_of_courses = Course.count
-
-    @uncompleted = Course.all
-
-    @completed = []
-    @badges.each do |badge|
-      @completed.push(Course.find(badge['course_id']))
-      @uncompleted -= [Course.find(badge['course_id'])]
-    end
   end
 end
